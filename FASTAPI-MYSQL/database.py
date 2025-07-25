e40553e0f8737d4fb6f7bb38925c1b388ac834a7
ShubhamKuttarmare25 <shubhamkuttarmare8329@gmail.com>
2025-07-25 10:57:23 +0530

    get endpoint to access user with user_id

--- FASTAPI-MYSQL/database.py
+++ FASTAPI-MYSQL/database.py
@@ -2,6 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
+
+#replace @ --> %40  ||  default localhost -> 3306
 URL_DATABASE = 'mysql+pymysql://root:Shubham%40123@localhost:3306/BlogApplication'
 
 engine = create_engine(URL_DATABASE)
--- FASTAPI-MYSQL/main.py
+++ FASTAPI-MYSQL/main.py
@@ -30,7 +30,7 @@ def get_db():
 db_dependency = Annotated[Session, Depends(get_db)]
 
 
-
+#post endpoint created 
 @app.post("/users/", status_code= status.HTTP_201_CREATED)
 async def create_user(user: UserBase, db: db_dependency):
     db_user = models.User(**user.dict())
@@ -38,3 +38,13 @@ async def create_user(user: UserBase, db: db_dependency):
     db.commit()
     db.refresh(db_user)
     return db_user
+
+
+#get endpoint to access user with user_id
+@app.get("/users/{user_id}", status_code = status.HTTP_200_OK)
+async def read_user(user_id: int, db: db_dependency):
+    user = db.query(models.User).filter(models.User.id == user_id).first()
+    if user is None:
+        raise HTTPException(status_code =404, detail = "User not found")
+    return user
+
